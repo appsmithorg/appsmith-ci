d99506a58df6ead258188741a85bcf6dcb68194b
Goutham Pratapa <goutham@appsmith.com>
2023-02-14 13:34:15 +0530

    Update cleanup_deploy_preview.sh

--- scripts/cleanup_deploy_preview.sh
+++ scripts/cleanup_deploy_preview.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-## Install relevant packages
-sudo apt update -q && sudo apt install -y curl unzip less jq
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl && \
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && \
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && \
-chmod 700 get_helm.sh; ./get_helm.sh
-
 mkdir ~/.aws; touch ~/.aws/config
 
 echo "[default]
